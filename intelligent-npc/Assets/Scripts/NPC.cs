4a2bd79c48ea05a204c02434c4b56c6e6c0c0d4b
wilberquito <wbq.software@gmail.com>
2021-10-29 18:45:51 +0200

    using heristic funtion of mlagents to move

--- intelligent-npc/Assets/Scripts/NPC.cs
+++ intelligent-npc/Assets/Scripts/NPC.cs
@@ -3,17 +3,14 @@ using System.Collections.Generic;
 using UnityEngine;
 using Unity.MLAgents;
 using Unity.MLAgents.Actuators;
+using Unity.MLAgents.Sensors;
 
 public class NPC : Agent
 {
-
     [SerializeField] float _movingRange = 5f;
-    [SerializeField] float velocity = 4f;
     [SerializeField] bool trainningMode = false;
     [SerializeField] float movementForce = 2f;
-
     [SerializeField] Transform currentTarget;
-
 
     Rigidbody2D _rigidbody2D;
     float gainObtein = 0f;
@@ -35,7 +32,6 @@ public class NPC : Agent
 
         // Debug.Log("Initialize...");
     }
-
 
     public override void OnEpisodeBegin()
     {
@@ -79,15 +75,41 @@ public class NPC : Agent
 
     // called when action is received from either {player, neural network}
     // each buffer position refers to an action, I decide what it means for each positions
-    // inside that structure has continuos and discrete actions
+    // inside that structure has continuos and continuos actions
     // index 0: -1 means move to the left, +1 means move to the right
     public override void OnActionReceived(ActionBuffers actions)
     {
-        Vector2 movement = new Vector2(actions.DiscreteActions[0], 0);
-
-        _rigidbody2D.AddForce(movement * movementForce);
-
+        Debug.Log("from on action received");
+        Debug.Log(actions.ContinuousActions[0]);
+        Vector2 movement = new Vector2(actions.ContinuousActions[0]*movementForce, 0);
+        _rigidbody2D.velocity = movement;
     }
+
+    // I observe the horientation of the NPC to the nereast target
+    // also I inform about the distace from the NPC to nearest target
+    public override void CollectObservations(VectorSensor sensor)
+    {
+
+        if (!currentTarget) return;
+
+        Vector2 toTarget = currentTarget.position - transform.position;
+        // 3 observations (horientation)
+        sensor.AddObservation(toTarget.normalized);
+        // 1 observation (distance)
+        sensor.AddObservation(Vector2.Distance(currentTarget.position, transform.position));
+    }
+
+    public override void Heuristic(in ActionBuffers actionsOut)
+    {
+        ActionSegment<float> continuosActions = actionsOut.ContinuousActions;
+        var force = Input.GetAxis("Horizontal");
+        // Debug.Log("from Heuristic");
+
+        // Debug.Log(force);
+
+        continuosActions[0] = force;
+    }
+
 
     // void Awake()
     // {
@@ -100,13 +122,10 @@ public class NPC : Agent
     //     // _rigidbody2D.velocity = new Vector2(0, 0);
     // }
 
-
     // void FixedUpdate()
     // {
-    //     Debug.Log();
-
-    //     // var force = Input.GetAxis("Horizontal");
-    //     // _rigidbody2D.velocity = new Vector2(force * velocity, 0);
+    //     var force = Input.GetAxis("Horizontal");
+    //     _rigidbody2D.velocity = new Vector2(force * velocity, 0);
     // }
 
     // private void OnTriggerEnter2D(Collider2D other) {
